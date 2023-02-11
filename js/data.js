aa9a26324f66cf7219f9445c219cf2b74506c3e7
E-Line21 <124870856+E-Line21@users.noreply.github.com>
2023-02-11 23:32:56 +0300

    Реализовано проигрывание музыки

--- js/data.js
+++ js/data.js
@@ -9,35 +9,43 @@ let albums = [
         tracks: [
             {
                 name: `Орём на весь район`,
-                time: `3:20`
+                time: `3:20`,
+                src: `assets/music/orjomNaVesRajjon.mp3`
             },
             {
                 name: `Табу`,
-                time: `4:20`
+                time: `4:20`,
+                src: `assets/music/tabu.mp3`
             },
             {
                 name: `Ни стыда, ни совести`,
-                time: `3:11`
+                time: `3:11`,
+                src: `assets/music/niStydaNiSovesti.mp3`
             },
             {
                 name: `Она горит на танцполе`,
-                time: `2:40`
+                time: `2:40`,
+                src: `assets/music/onaGoritNaTancpole.mp3`
             },
             {
                 name: `Снимай с себя всё`,
-                time: `3:16`
+                time: `3:16`,
+                src: `assets/music/snimajjSSebyaVsjo.mp3`
             },
             {
                 name: `Она яд`,
-                time: `4:16`
+                time: `4:16`,
+                src: `assets/music/onaYad.mp3`
             },
             {
                 name: `Чернилами`,
-                time: `3:48`
+                time: `3:48`,
+                src: `assets/music/chernilami.mp3`
             },
             {
-                name: `Чернилами`,
-                time: `3:48`
+                name: `Девочка хочет движа`,
+                time: `3:12`,
+                src: `assets/music/devochkaKhochetDvizha.mp3`
             }
         ]
     },
@@ -51,27 +59,33 @@ let albums = [
         tracks: [
             {
                 name: `Лабиринты`,
-                time: `3:20`
+                time: `3:20`,
+                src: `assets/music/labirinty.mp3`
             },
             {
                 name: `Путеводная`,
-                time: `3:13`
+                time: `3:13`,
+                src: `assets/music/putevodnaya.mp3`
             },
             {
                 name: `ЧВЭП`,
-                time: `2:50`
+                time: `2:50`,
+                src: `assets/music/chvejep.mp3`
             },
             {
                 name: `Не люблю`,
-                time: `2:39`
+                time: `2:39`,
+                src: `assets/music/neLyublyu.mp3`
             },
             {
                 name: `Помада`,
-                time: `3:19`
+                time: `3:19`,
+                src: `assets/music/pomada.mp3`
             },
             {
                 name: `Панацея`,
-                time: `3:05`
+                time: `3:05`,
+                src: `assets/music/panaceya.mp3`
             }
         ]
     },
@@ -85,7 +99,8 @@ let albums = [
         tracks: [
             {
                 name: `По садовому`,
-                time: `2:42`
+                time: `2:42`,
+                src: `assets/music/poSadovomu.mp3`
             }
         ]
     },
@@ -99,7 +114,8 @@ let albums = [
         tracks: [
             {
                 name: `Даже не половина`,
-                time: `2:51`
+                time: `2:51`,
+                src: `assets/music/dazheNePolovina.mp3`
             }
         ]
     }
--- js/play-list.js
+++ js/play-list.js
@@ -30,10 +30,81 @@ for (let j = 0; j < tracks.length; j++) {
     document
         .querySelector(`.tracks`)
         .innerHTML += `
-        <li class="list-group-item d-flex align-items-center">
-            <img src="assets/play-list/paused.svg" alt="Иконка начала воспроизведния музыки" class="playing me-2" height="30px">
+        <li class="list-group-item d-flex align-items-center track">
+            <audio class="audio paused" src="${tracks[j].src}"></audio>
+            <button class="button">
+                <img src="assets/play-list/paused.svg" alt="Иконка начала воспроизведния музыки" class="icon playing me-2" height="30px">
+            </button>
             <span>${tracks[j].name}</span>
             <span class="time ms-auto me-2 text-muted">${tracks[j].time}</span>
         </li>
         `;
+}
+
+setupAudio();
+
+
+
+function pauseAll() {
+    let tracksNode = document.querySelectorAll(`.track`)
+    for (let i = 0; i < tracksNode.length; i++) {
+        tracksNode[i]
+            .querySelector(`.audio`)
+            .pause();
+        tracksNode[i]
+            .querySelector(`.audio`)
+            .classList
+            .remove(`playing`);
+        tracksNode[i]
+            .querySelector(`.audio`)
+            .classList
+            .add(`paused`);
+        tracksNode[i]
+            .querySelector(`.icon`)
+            .src = `assets/play-list/paused.svg`;
+    }
+}
+
+function setupAudio() {
+    let tracksNode = document.querySelectorAll(`.track`);
+    for (let j = 0; j < tracksNode.length; j++) {
+        let btn = tracksNode[j].querySelector(`.button`);
+        let audio = tracksNode[j].querySelector(`.audio`);
+        let img = tracksNode[j].querySelector(`.icon`);
+        let time = tracksNode[j].querySelector(`.time`);
+        function updateProgress() {
+            // Нарисовать актуальное время
+            time.innerHTML = getTime(audio.currentTime);
+          
+            // Нужно ли вызвать её ещё раз?
+            if (audio.classList.contains(`playing`)) {
+                requestAnimationFrame(updateProgress);
+            } else {
+                time.innerHTML = getTime(audio.duration);
+            }
+            
+        }
+        
+        btn.addEventListener(`click`, function() {
+            if (audio.classList.contains(`paused`)) {
+                pauseAll();
+                audio.classList.remove(`paused`);
+                audio.classList.add(`playing`);
+                audio.play();
+                updateProgress();
+                img.src = `assets/play-list/plaing.svg`
+            } else {
+                audio.classList.add(`paused`);
+                audio.classList.remove(`playing`);
+                audio.pause();
+                img.src = `assets/play-list/paused.svg`;
+            }
+        });
+    }
+}
+
+function getTime(time) {
+    let min = Math.floor(time/60);
+    let sec = Math.floor(time)%60;
+    return `${min}:${sec}`
 }
